ba2839c04c10247b351f26010fa4ad4fa05f17f8
ViktorSorokin7500 <berhaneselassie1945@gmail.com>
2025-09-18 16:48:05 +0300

    back to content

--- app/[lang]/(root)/(pages)/dashboard/page.tsx
+++ app/[lang]/(root)/(pages)/dashboard/page.tsx
@@ -1,21 +1,12 @@
-import { DashboardSkeleton } from "@/components/dashboard";
+import { DashboardContent, DashboardSkeleton } from "@/components/dashboard";
+import { Suspense } from "react";
+
 export default function Dashboard() {
   return (
     <div className="bg-orange-50 p-2 sm:p-8 min-h-screen">
-      <DashboardSkeleton />
+      <Suspense fallback={<DashboardSkeleton />}>
+        <DashboardContent />
+      </Suspense>
     </div>
   );
 }
-
-// import { DashboardContent, DashboardSkeleton } from "@/components/dashboard";
-// import { Suspense } from "react";
-
-// export default function Dashboard() {
-//   return (
-//     <div className="bg-orange-50 p-2 sm:p-8 min-h-screen">
-//       <Suspense fallback={<DashboardSkeleton />}>
-//         <DashboardContent />
-//       </Suspense>
-//     </div>
-//   );
-// }
