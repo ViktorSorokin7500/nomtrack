a1de9e96638fe72252e569528719274ef7e932a9
ViktorSorokin7500 <berhaneselassie1945@gmail.com>
2025-09-18 16:28:18 +0300

    skeleton try

--- app/[lang]/(root)/(pages)/dashboard/page.tsx
+++ app/[lang]/(root)/(pages)/dashboard/page.tsx
@@ -1,12 +1,21 @@
-import { DashboardContent, DashboardSkeleton } from "@/components/dashboard";
-import { Suspense } from "react";
-
+import { DashboardSkeleton } from "@/components/dashboard";
 export default function Dashboard() {
   return (
     <div className="bg-orange-50 p-2 sm:p-8 min-h-screen">
-      <Suspense fallback={<DashboardSkeleton />}>
-        <DashboardContent />
-      </Suspense>
+      <DashboardSkeleton />
     </div>
   );
 }
+
+// import { DashboardContent, DashboardSkeleton } from "@/components/dashboard";
+// import { Suspense } from "react";
+
+// export default function Dashboard() {
+//   return (
+//     <div className="bg-orange-50 p-2 sm:p-8 min-h-screen">
+//       <Suspense fallback={<DashboardSkeleton />}>
+//         <DashboardContent />
+//       </Suspense>
+//     </div>
+//   );
+// }
